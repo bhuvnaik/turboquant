fcedf3ea53f1f3ec31e90a26aff9c09c2cff8ebb
Bhuvan Naik <144937979+bhuvnaik@users.noreply.github.com>
2026-05-09 20:10:29 +0530

    Update rotation.py

--- core/rotation.py
+++ core/rotation.py
@@ -7,7 +7,7 @@ class RandomRotation:
 
         A = torch.randn(d, d, device=device)
         Q, _ = torch.linalg.qr(A)
-        self.P = Q  # orthogonal matrix
+        self.P = Q  
 
     def apply(self, x):
         # x: (batch, d)
